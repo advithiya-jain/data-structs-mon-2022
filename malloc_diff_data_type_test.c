dd9f85f89bd963f12a78e657c29f462ee00b674f
advithiya-jain <advithiyajain1@gmail.com>
2022-09-24 16:08:10 +0530

    Updated to count for char* memory cap
    Character pointer arrays cap out at 512 bytes, so instead of making a 1000 index int* array, I updated it to a 100 index int* array.

--- malloc_diff_data_type_test.c
+++ malloc_diff_data_type_test.c
@@ -5,22 +5,23 @@ int main(int argc, char** argv){
     int *a;
     char *b;
     int i;
-    a = malloc(1000 * sizeof(int));
-   // printf("%d\n", *(a+1001)); 
+    //Using 100 instead of 1000 because character pointer arrays cap out at 512 indeces. (Could use 127 too)
+    a = malloc(100 * sizeof(int));
    // can also be written as a[999] or a[0] 
    printf("All vaules of a:\n\n");
-   for (i = 0; i < 1000; i++){
+   for (i = 0; i < 100; i++){
         a[i] = i;
         printf("%d\n", a[i]);
    }
     b = (char*)a;
     i = 0;
    printf("\nAll vaules of a accessed through b (character pointer):\n\n");
-    while(i<3997){
+   // 397 is 99*4 since we have to move 4 locations (characters use 1 byte while integers use 4)
+    while(i<397){
         printf("%d\n", *(b+i));
-        i+=4;
+        i=i+4; // incrementing i by 4 (same reason as above).
     }
     //getting a weird output where it goes from 0 to 127 then -128, -127, -126... back to 0
-    //this cycle continues till the 999th index
+    //this cycle continues till the 999th index - this is if the integer pointer array has 100 indeces.
     free(a);
 }
