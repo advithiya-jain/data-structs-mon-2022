8fae1c59e9b15c4145767f50ee79127be865dea7
advithiya-jain <advithiyajain1@gmail.com>
2022-09-24 18:20:12 +0530

    Updated decimal adder into smaller numbers
    Changed the values of the decimals because the error was not showing up. Still doesn't show up on my M1Pro chipset, but it may in others. Will test this on the DS server.

--- class-and-test-programs/decimal_adder_check.c
+++ class-and-test-programs/decimal_adder_check.c
@@ -2,7 +2,10 @@
 
 int main(){
   float sum = 0;
-  sum = 0.1 + 0.1 + 0.1;
+  sum = (0.00001) + 2.000051;
+  // Tried with the smallest numbers I could.
+  // Still couldnt get the error to show up for me. 
+  // Changes can be seen in the commit history.
   printf("%f\n", sum);
   return 0;
 }
