f75c54e79bc419f4e0a8795cafcc4a49195830c8
advithiya-jain <advithiyajain1@gmail.com>
2022-09-26 08:51:27 +0000

    Edited structs file more to add while loop

--- class-and-test-programs/structs_and_sp_data_types.c
+++ class-and-test-programs/structs_and_sp_data_types.c
@@ -3,10 +3,15 @@
 
 int main(int argc, char **argv) {
     int *a;
-    int i, n;
+    int i, n val;
 
     // Enter Data
-
+    scanf("%d", &val);
+    while(val > 0) {
+        // do stuff
+        
+        scanf("%d", &val)
+    }
     // Print Data
     for(i = 0; i < n; i++) {
         printf("%d", a[i]);
