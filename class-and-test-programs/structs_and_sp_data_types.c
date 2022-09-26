663624dfa51c3e941a6421373c0f670f69b9ee84
advithiya-jain <advithiyajain1@gmail.com>
2022-09-26 08:53:22 +0000

    added some code into the while loop in structs

--- class-and-test-programs/structs_and_sp_data_types.c
+++ class-and-test-programs/structs_and_sp_data_types.c
@@ -3,13 +3,17 @@
 
 int main(int argc, char **argv) {
     int *a;
-    int i, n val;
+    int i, n, val;
+    n = 3;
+    a = (int *) malloc(n * sizeof(int));
 
     // Enter Data
     scanf("%d", &val);
+    i = 0
     while(val > 0) {
         // do stuff
-        
+        a[i] = val;
+        i++;
         scanf("%d", &val)
     }
     // Print Data
