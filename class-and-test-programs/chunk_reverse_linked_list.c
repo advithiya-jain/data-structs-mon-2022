87a38699b1ea3614ebe9d01d95ddab8dbf279faf
advithiya-jain <advithiyajain1@gmail.com>
2022-10-09 17:36:37 +0000

    Updated program to chunk reverse linked list
    Added algorithm and function to chunk reverse, checked for edge cases as well. Program works at least to the best of my testing.

--- class-and-test-programs/chunk_reverse_linked_list.c
+++ class-and-test-programs/chunk_reverse_linked_list.c
@@ -19,6 +19,7 @@ void printLinked(Node** head) {
         temp = temp->next;
         i++;
     }
+    printf(". :D\n");
 }
 
 //Function to free a Linked list
@@ -75,30 +76,57 @@ void userLinkedList(Node** head, int n){
     }
 }
 
-// Function to reverse a given linked list
-Node* reverseLinkedList(Node **head_addr) {
-    // initialising nodeType variables
-    Node* prevNode = NULL;
-    Node* nextNode = NULL;
-    Node* currentNode = *head_addr;
-
-    while(currentNode != NULL) {
-        nextNode = currentNode->next;
-        currentNode->next = prevNode;
-        prevNode = currentNode;
-        currentNode = nextNode;
-    }
-    (*head_addr) = prevNode;
-    return (*head_addr);
-}
-
 /*TODO: Chunk reverse a linked list where the chunk size 'k' is input by the user.
         Ex: input: 1->2->3->4->5->6->7->8->9->10->11->NULL, k = 3
            output: 3->2->1->6->5->4->9->8->7->11->10->NULL
 
         Algorithm:
+                    # Initialise nodes to be used
+                    Node* prevNode, nextNode = NULL
+                    Node* currentNode = head;
+                    # Initialise counter variable to 0 
+                    # (used to check how many nodes have been reversed according to chunk size)
+                    ctr = 0
+                    while(currentNode != NULL AND ctr<k):
+                            # reversal logic, see: reverse_linked_list.c
+                            nextNode = currentNode->next;
+                            currentNode->next = prevNode;
+                            prevNode = currentNode;
+                            currentNode = nextNode;
+                            #increment counter by one to indicate a reversal
+                            ctr++;
+                    
+                    # Outside while loop. next points to the element at address k+1 
+                    # so we set the new head->next to a recursive call to this function, passing
+                    # nextNode as the head this time.
+                    if(nextNode != NULL):
+                        head->next = chunkreverse(nextNode, k)
+                    # after recursive calls, the previous node now points to the start of list 
+                    # (see: reverse_linked_list.c) so we return the previous Node
+                    return prevNode
                     
 */
+Node* chunkReverseLinkedList(Node *head_addr, int k) {
+    // initialising nodeType variables
+    Node* prevNode = NULL;
+    Node* nextNode = NULL;
+    Node* currentNode = head_addr;
+    int ctr = 0;
+
+    while(currentNode != NULL && ctr < k) {
+        nextNode = currentNode->next;
+        currentNode->next = prevNode;
+        prevNode = currentNode;
+        currentNode = nextNode;
+        ctr++;
+    }
+    
+    if(nextNode != NULL)
+        head_addr->next = chunkReverseLinkedList(nextNode, k);
+    
+    return prevNode;
+}
+
 
 int main(int argc, char** argv) {
     //Initialise variables to be used
@@ -115,15 +143,34 @@ int main(int argc, char** argv) {
         }
         else {
             printf("Enter chuck size: ");
+            scanf("%d", &k);
+            if(k < 0){
+                printf("Invalid chunk size >:(\n");
+                flag = 1; // set flag to 1 so the loop repeats.
+            }
+            else {
+                flag = 0;
+            }
         }
     }while(flag == 1);
     //calling userLinkedList function to take user input for values in the linked list.
     userLinkedList(&linked_list, n);
     //printing the created linked llist
-    printf("Congratulations! Your created linked list is: ");
+    printf("Your created linked list is: ");
     printLinked(&linked_list);
-    // Print statement for aesthetics
-    printf(". :D \n");
+    // Printing prompt before printing the reversed linked list
+    printf("Congratulations! Your chunk reversed list is: ");
+    //Checking if k = 0, if so just printing the linked list as it is.
+    if(k == 0){
+        printLinked(&linked_list);
+    }
+    else {
+        //chunk reversing the list using the chunkReverseLinkedList() function
+        linked_list = chunkReverseLinkedList(linked_list, k);
+        // Printing the chunk reversed linked list.
+        printLinked(&linked_list);
+    }
+    // freeing space used up by the linked list.
     freeLinkedList(linked_list);
     return 0;
 }
