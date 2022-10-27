9f08cb48e4a88d4a832a5fe27ea299f35667d9b0
advithiya-jain <advithiyajain1@gmail.com>
2022-10-27 19:04:41 +0000

    Initial commit for stack implementation, have to still write the code

---
+++ own-random-progs/stack_implementation.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<time.h>
+
+/*************************************************** FOR STACKS *****************************************************/
+typedef struct node {
+    int val;
+    struct node* next;
+}node;
+
